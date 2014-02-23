e69e672e90f57938496d35be977d5a2438d546c0
Steven Loria <sloria1@gmail.com>
2014-02-22 19:47:57 -0500

    remove distutils from setup.py

--- setup.py
+++ setup.py
@@ -5,37 +5,7 @@ import os
 import re
 import subprocess
 
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from distutils.core import setup
-    from distutils.util import convert_path
-
-    def _find_packages(where='.', exclude=()):
-        """Return a list all Python packages found within directory 'where'
-
-        'where' should be supplied as a "cross-platform" (i.e. URL-style) path; it
-        will be converted to the appropriate local path syntax.  'exclude' is a
-        sequence of package names to exclude; '*' can be used as a wildcard in the
-        names, such that 'foo.*' will exclude all subpackages of 'foo' (but not
-        'foo' itself).
-        """
-        out = []
-        stack = [(convert_path(where), '')]
-        while stack:
-            where, prefix = stack.pop(0)
-            for name in os.listdir(where):
-                fn = os.path.join(where, name)
-                if ('.' not in name and os.path.isdir(fn) and
-                        os.path.isfile(os.path.join(fn, '__init__.py'))):
-                    out.append(prefix+name)
-                    stack.append((fn, prefix + name + '.'))
-        for pat in list(exclude)+['ez_setup', 'distribute_setup']:
-            from fnmatch import fnmatchcase
-            out = [item for item in out if not fnmatchcase(item, pat)]
-        return out
-
-    find_packages = _find_packages
+from setuptools import setup, find_packages
 
 def find_version(fname):
     '''Attempts to find the version number in the file names fname.
