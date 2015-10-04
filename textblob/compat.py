daf81ee75d674a67c951108768d636f19e39b338
Steven Loria <sloria1@gmail.com>
2015-10-04 18:29:12 -0400

    Improve with_metaclass

--- textblob/compat.py
+++ textblob/compat.py
@@ -46,21 +46,14 @@ else: # PY3
     implements_to_string = lambda x: x
 
 
+# From six
 def with_metaclass(meta, *bases):
-    """Defines a metaclass.
+    """Create a base class with a metaclass."""
+    # This requires a bit of explanation: the basic idea is to make a dummy
+    # metaclass for one level of class instantiation that replaces itself with
+    # the actual metaclass.
+    class metaclass(meta):  # noqa
 
-    Creates a dummy class with a dummy metaclass. When subclassed, the dummy
-    metaclass is used, which has a constructor that instantiates a
-    new class from the original parent. This ensures that the dummy class and
-    dummy metaclass are not in the inheritance tree.
-
-    Credit to Armin Ronacher.
-    """
-    class metaclass(meta):
-        __call__ = type.__call__
-        __init__ = type.__init__
         def __new__(cls, name, this_bases, d):
-            if this_bases is None:
-                return type.__new__(cls, name, (), d)
             return meta(name, bases, d)
-    return metaclass('temporary_class', None, {})
+    return type.__new__(metaclass, 'temporary_class', (), {})
