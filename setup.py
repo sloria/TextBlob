f5147d1ce2eb3e1ab701362fd11398cb37e8b37c
Steven Loria <sloria1@gmail.com>
2015-10-04 19:13:23 -0400

    Not quite ready for py35
    Until https://github.com/nltk/nltk/issues/1106
    is released

--- setup.py
+++ setup.py
@@ -57,7 +57,6 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         "Topic :: Text Processing :: Linguistic",
