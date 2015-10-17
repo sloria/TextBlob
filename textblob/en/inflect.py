7069c1f6c533622a493584dd9d9cd93e5f1fcbbe
Jon Coe <jonathon.coe@offerpop.com>
2015-10-17 18:52:47 -0400

    remove dupe 'glottis' from s-singular list

--- textblob/en/inflect.py
+++ textblob/en/inflect.py
@@ -189,7 +189,7 @@ plural_categories = {
         "sand", "software", "understanding", "water"],
     "s-singular": [
         "acropolis", "aegis", "alias", "asbestos", "bathos", "bias", "caddis", "cannabis", "canvas",
-        "chaos", "cosmos", "dais", "digitalis", "epidermis", "ethos", "gas", "glottis", "glottis",
+        "chaos", "cosmos", "dais", "digitalis", "epidermis", "ethos", "gas", "glottis",
         "ibis", "lens", "mantis", "marquis", "metropolis", "pathos", "pelvis", "polis", "rhinoceros",
         "sassafras", "trellis"],
     "ex-ices": ["codex", "murex", "silex"],
