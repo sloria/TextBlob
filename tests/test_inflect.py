650dc552358c7d462e0c52020e560aa1044fd82e
Jon Coe <jonathon.coe@offerpop.com>
2015-10-17 19:03:17 -0400

    test for plural (lenses) --> singular (lens). fails

--- tests/test_inflect.py
+++ tests/test_inflect.py
@@ -5,6 +5,10 @@ from textblob import Word
 
 class PluralizationTestCase(TestCase):
 
-    def s_singular_test(self):
+    def s_singular__pluralize_test(self):
         lens = Word('lens')
         self.assertEquals(lens.pluralize(), 'lenses')
+
+    def s_singular_singularize_test(self):
+        lenses = Word('lenses')
+        self.assertEquals(lenses.singularize(), 'lens')
