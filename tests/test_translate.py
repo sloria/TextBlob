63aae65f2e7dfc9bf5230949a62ab4ed2c902058
Steven Loria <sloria1@gmail.com>
2016-02-17 19:58:59 -0500

    Fix translation test

--- tests/test_translate.py
+++ tests/test_translate.py
@@ -91,7 +91,7 @@ class TestTranslatorIntegration(unittest.TestCase):
     def test_translate_text(self):
         text = "This is a sentence."
         translated = self.translator.translate(text, to_lang="es")
-        assert_equal(translated, "Esta es una oración.")
+        assert_equal(translated, "Esta es una frase.")
         es_text = "Esta es una frase."
         to_en = self.translator.translate(es_text, from_lang="es", to_lang="en")
         assert_equal(to_en, "This is a sentence.")
