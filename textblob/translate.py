cbae51c0b7732baf3242066dee4d3aecf6f40a4e
Jhon Eslava <epicjhon1994@gmail.com>
2017-01-07 20:56:23 -0500

    Fix TK

--- textblob/translate.py
+++ textblob/translate.py
@@ -11,7 +11,6 @@ import codecs
 import ctypes
 import json
 import re
-import time
 
 from textblob.compat import PY2, request, urlencode
 from textblob.exceptions import TranslatorError, NotTranslated
@@ -31,7 +30,7 @@ class Translator(object):
         u'es'
     """
 
-    url = "http://translate.google.com/translate_a/t?client=webapp"
+    url = "http://translate.google.com/translate_a/t?client=webapp&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&dt=at&ie=UTF-8&oe=UTF-8&otf=2&ssel=0&tsel=0&kc=1"
 
     headers = {
         'Accept': '*/*',
@@ -45,11 +44,9 @@ class Translator(object):
         """Translate the source text from one language to another."""
         if PY2:
             source = source.encode('utf-8')
-        data = {"client": "p",
-                "ie": "UTF-8", "oe": "UTF-8",
-                "dt": "at", "tk": _calculate_tk(source),
-                "sl": from_lang, "tl": to_lang, "text": source}
-        response = self._request(self.url, host=host, type_=type_, data=data)
+        data = {"q": source}
+        url = self.url + "&sl=" + from_lang + "&tl=" + to_lang + "&hl=" + to_lang + "&tk=" + _calculate_tk(source)
+        response = self._request(url, host=host, type_=type_, data=data)
         result = json.loads(response)
         if isinstance(result, list):
             try:
@@ -65,11 +62,9 @@ class Translator(object):
             source = source.encode('utf-8')
         if len(source) < 3:
             raise TranslatorError('Must provide a string with at least 3 characters.')
-        data = {"client": "p",
-                "ie": "UTF-8", "oe": "UTF-8",
-                "dt": "at", "tk": _calculate_tk(source),
-                "sl": "auto", "text": source}
-        response = self._request(self.url, host=host, type_=type_, data=data)
+        data = {"q": source}
+        url = self.url + "&sl=auto&tk=" + _calculate_tk(source)
+        response = self._request(url, host=host, type_=type_, data=data)
         result, language = json.loads(response)
         return language
 
@@ -105,7 +100,10 @@ def _unescape(text):
 def _calculate_tk(source):
     """Reverse engineered cross-site request protection."""
     # Source: https://github.com/soimort/translate-shell/issues/94#issuecomment-165433715
-    b = int(time.time() / 3600)
+    # Source: http://www.liuxiatool.com/t.php
+
+    tkk = [406398, 561666268 + 1526272306]
+    b = tkk[0]
 
     if PY2:
         d = map(ord, source)
@@ -114,17 +112,20 @@ def _calculate_tk(source):
 
     def RL(a, b):
         for c in range(0, len(b) - 2, 3):
-            d = b[c+2]
+            d = b[c + 2]
             d = ord(d) - 87 if d >= 'a' else int(d)
             xa = ctypes.c_uint32(a).value
-            d = xa >> d if b[c+1] == '+' else xa << d
+            d = xa >> d if b[c + 1] == '+' else xa << d
             a = a + d & 4294967295 if b[c] == '+' else a ^ d
         return ctypes.c_int32(a).value
 
     a = b
+
     for di in d:
         a = RL(a + di, "+-a^+6")
+
     a = RL(a, "+-3^+b+-f")
+    a ^= tkk[1]
     a = a if a >= 0 else ((a & 2147483647) + 2147483648)
     a %= pow(10, 6)
 
