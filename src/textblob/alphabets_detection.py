c5a751126041002fe047feeb90cd1f1223045110
mikebgrep <72438723+mikebgrep@users.noreply.github.com>
2025-04-24 21:49:54 +0300

    Update alphabets_detection.py

--- src/textblob/alphabets_detection.py
+++ src/textblob/alphabets_detection.py
@@ -130,5 +130,5 @@ def detect_alphabets(text: str):
         if progress >= 99.99:
             return result
 
-    result.append((Alphabet.STRING_CONTAINS_NOT_IMPLEMENTED_ALPHABET.name, 0))
+    result.append((Alphabet.STRING_CONTAINS_NOT_IMPLEMENTED_ALPHABET.name, round(100 - progress, 2))
     return result
